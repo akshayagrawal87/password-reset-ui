9865aa7b61977715505186662fd23578f87b0438
akshayagrawal87 <akshayagrawal87@gmail.com>
2020-12-02 00:34:29 +0530

    added ui for password reset

---
+++ index.js
@@ -0,0 +1,48 @@
+let loginUrl = "http://localhost:8000/login";
+
+$(".alert").hide();
+
+let email = document.getElementById("username");
+
+let password = document.getElementById("password");
+
+let loginButton = document.querySelector(".loginButton");
+(async () => {
+	let form = document.querySelector("#loginForm");
+
+	form.addEventListener("input", (e) => {
+		if (email.value.length > 0 && password.value.length > 0) {
+			loginButton.removeAttribute("disabled");
+		} else {
+			loginButton.setAttribute("disabled", "disabled");
+		}
+	});
+
+	form.addEventListener("submit", checkOnSubmit, true);
+})();
+
+async function checkOnSubmit() {
+	let body = {
+		username: email.value,
+		password: password.value,
+	};
+
+	const response = await fetch(loginUrl, {
+		method: "POST",
+		body: JSON.stringify(body),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	let data = await response.json();
+
+	if (data.loggedIn) {
+		location.href = "welcome.html";
+	} else {
+		$(".alert").show();
+		setTimeout(() => {
+			$(".alert").hide();
+		}, 9000);
+	}
+}
---
+++ signUp.js
@@ -0,0 +1,51 @@
+let registerUrl = "http://localhost:8000/register";
+
+$(".alert").hide();
+
+let email = document.getElementById("username");
+
+let password = document.getElementById("password");
+
+let registerButton = document.querySelector(".registerButton");
+(async () => {
+	let form = document.querySelector("#signUpForm");
+
+	form.addEventListener("input", (e) => {
+		if (email.value.length > 0 && password.value.length > 0) {
+			registerButton.removeAttribute("disabled");
+		} else {
+			registerButton.setAttribute("disabled", "disabled");
+		}
+	});
+
+	form.addEventListener("submit", checkOnSubmit, true);
+})();
+
+async function checkOnSubmit() {
+	let body = {
+		username: email.value,
+		password: password.value,
+	};
+
+	const response = await fetch(registerUrl, {
+		method: "POST",
+		body: JSON.stringify(body),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	let data = await response.json();
+
+	if (data.created) {
+		location.href = "index.html";
+	} else {
+		let alertMesssge = document.querySelector(".alertMessage");
+		alertMesssge.innerHTML = data.message;
+
+		$(".alert").show();
+		setTimeout(() => {
+			$(".alert").hide();
+		}, 9000);
+	}
+}
